fa446af04644257e3d33821356b2ea97b7da472b
Lilian VERLHAC <lilian.verlhac@epitech.eu>
2021-03-13 20:39:41 +0100

    [FIX] Bad uri middleware condition

--- API/spotify/middlewares/verify.player.middleware.ts
+++ API/spotify/middlewares/verify.player.middleware.ts
@@ -2,7 +2,7 @@ import { NextFunction, Request, Response } from 'express'
 import ErrorModel from '../../common/models/error.model'
 
 export default function hasPlayerValidArguments(req: Request, res: Response<ErrorModel>, next: NextFunction): Response<ErrorModel> {
-    if (req.query && !req.query.uri) {
+    if (req.query && req.query.uri) {
         next()
     } else {
         return res.status(400).send(new ErrorModel(req.url, 'Missing uri argument'))
