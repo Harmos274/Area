9b7986c964cd1523e7f74c32a2e4c01e8d136213
Lilian VERLHAC <lilian.verlhac@epitech.eu>
2021-03-13 20:38:25 +0100

    [FIX] Uri middleware for clean code

--- API/spotify/middlewares/verify.player.middleware.ts
+++ API/spotify/middlewares/verify.player.middleware.ts
@@ -2,12 +2,9 @@ import { NextFunction, Request, Response } from 'express'
 import ErrorModel from '../../common/models/error.model'
 
 export default function hasPlayerValidArguments(req: Request, res: Response<ErrorModel>, next: NextFunction): Response<ErrorModel> {
-    if (req.query) {
-        if (!req.query.uri) {
-            res.status(400).send(new ErrorModel(req.url, 'Missing sub argument'))
-        }
+    if (req.query && !req.query.uri) {
         next()
     } else {
-        return res.status(400).send(new ErrorModel(req.url, 'Missing url arguments'))
+        return res.status(400).send(new ErrorModel(req.url, 'Missing uri argument'))
     }
 }
